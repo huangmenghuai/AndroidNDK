902458bc1fc7cc256a74a88eeda946249035c678
HUANG Menghuai <linker.huang@gmail.com>
2014-08-20 09:50:18 +0800

    Create ndk-env.sh

---
+++ ndk-env.sh
@@ -0,0 +1,4 @@
+export SYSROOT=/media/sda4/adt/NDK/android-ndk-r9d/platforms/android-17/arch-arm
+export CFLAGS=--sysroot=$SYSROOT
+export LDFLAGS=--sysroot=$SYSROOT
+export PATH=/media/sda4/adt/NDK/android-ndk-r9d/toolchains/arm-linux-androideabi-4.6/prebuilt/linux-x86_64/bin:$PATH
